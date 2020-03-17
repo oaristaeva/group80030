542d2353698dc816d1cdedb00d445a53e8b4bec1
oaristaeva <oaristaeva@pier1.com>
2020-03-17 15:10:50 +0300

    changed

--- oaristaeva/HelloWorld.java
+++ oaristaeva/HelloWorld.java
@@ -1,6 +1,6 @@
 package group80030.oaristaeva;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("H e l l o World!");
+        System.out.println("Hello World!");
     }
 }
